514c84cf35af659bb2623e13effeb191e81592c6
JHEEE116 <sjhyt0813@gmail.com>
2025-04-05 19:49:58 +0900

    2회차과제-손재희

---
+++ src/Lion/Main.java
@@ -0,0 +1,10 @@
+package Lion;
+
+//메인문 클래스
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("yu-lion-management_system\n");
+        Yu_Lion_Management lion = new Yu_Lion_Management();
+        lion.Menu();
+    }
+}
