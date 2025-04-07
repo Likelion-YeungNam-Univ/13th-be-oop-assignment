766b1b9b05874132c4e3bc6293218a57e0ac12a7
chamingyeong <mingyeong1104@naver.com>
2025-04-08 01:06:17 +0900

    add hw

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,38 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Menu menu = new Menu();
+        Scanner scanner = new Scanner(System.in);
+        Manage manage = new Manage();
+
+        int num;
+        while (true){
+            menu.print_menu();
+            System.out.print("원하는 기능 선택 : ");
+            num = scanner.nextInt();
+            switch(num){
+                case 1:
+                    manage.register();
+                    break;
+                case 2:
+                    manage.allInquiry();
+                    break;
+                case 3:
+                    manage.individualInquiry();
+                    break;
+                case 4:
+                    manage.modifyIntroduction();
+                    break;
+                case 5:
+                    manage.modifyFaithful();
+                    break;
+                case 6:
+                    manage.delete();
+                default:
+            }
+            System.out.println();
+        }
     }
 }
